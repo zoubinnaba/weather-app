70baf51990449da3aead85c4d850b3fe01987530
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2022-04-16 10:45:11 +0000

    create weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const apiKey = 'f8e109c6bb43bea8bdc7773508600066'
+    const [weatherData, setWeatherData] = useState([{}])
+    const [city, setCity] = useState("")
+
+    const getWeater = (event) => {
+      if(event.key === "Enter"){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).
+        then(response => response.json()
+        ).then(data => {
+          setWeatherData(data)
+          setCity("")
+        })
+      }
+    }
+
+    return (
+        <div className="container">
+            <input 
+            className="input" 
+            placeholder="Enter city..."
+            onChange={e => setCity(e.target.value)}
+            value={city}
+            onKeyPress={getWeater}
+            />
+            {typeof weatherData.main === 'undefined' ? (
+              <div>
+                <p>Welcome to weather app! Enter a city name. </p>
+              </div>
+            ): (
+              <div className='weather-data'>
+                <p className='city-name'>{ weatherData.name } </p>
+                <p className='city-temp'>{ Math.round(weatherData.main.temp)} 🌡️</p>
+                <p className='city-hum'>{ Math.round(weatherData.main.humidity)}</p>
+                <p className='cloud'>{weatherData.weather[0].main}</p>
+              </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
